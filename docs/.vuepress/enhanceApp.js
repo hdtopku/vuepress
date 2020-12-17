bc2bbed3d93a61cfb6a23c23f5a56aa405f31122
hdtopku <1450948930@qq.com>
2020-12-17 21:12:37 +0800

    revert 临时登录功能

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -8,35 +8,35 @@ export default ({
                   siteData
                 }) => {
   Vue.mixin({
-    beforeMount() {
-      const doCheck = () => {
-        if (!checkAuth()) {
-          this.$dlg.modal(Login, {
-            width: 500,
-            height: 350,
-            title: '用户登录',
-            singletonKey: 'employee-login',
-            maxButton: false,
-            closeButton: false,
-            callback: data => {
-              if (data === true) {
-                // do some stuff after login
-              }
-            }
-          })
-        }
-      }
-
-      if (this.$dlg) {
-        doCheck()
-      } else {
-        import('v-dialogs').then(resp => {
-          Vue.use(resp.default)
-          this.$nextTick(() => {
-            doCheck()
-          })
-        })
-      }
-    }
+    // beforeMount() {
+    //   const doCheck = () => {
+    //     if (!checkAuth()) {
+    //       this.$dlg.modal(Login, {
+    //         width: 500,
+    //         height: 350,
+    //         title: '用户登录',
+    //         singletonKey: 'employee-login',
+    //         maxButton: false,
+    //         closeButton: false,
+    //         callback: data => {
+    //           if (data === true) {
+    //             // do some stuff after login
+    //           }
+    //         }
+    //       })
+    //     }
+    //   }
+    //
+    //   if (this.$dlg) {
+    //     doCheck()
+    //   } else {
+    //     import('v-dialogs').then(resp => {
+    //       Vue.use(resp.default)
+    //       this.$nextTick(() => {
+    //         doCheck()
+    //       })
+    //     })
+    //   }
+    // }
   })
 }
